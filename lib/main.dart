49af2f09207b01c1bfb76375a4314d0730f827f4
Filip Bartosiak <filipbart1@gmail.com>
2020-12-05 13:17:32 +0100

    First pull - app base
    Quiz from udemy course, first repo pull

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(QuizApp());
+
+class QuizApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Quiz App'),
+        ),
+        body: Text('Default text'),
+      ),
+    );
+  }
+}
