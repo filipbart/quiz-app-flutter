23092b135e43822978227ce13bfc775288801bde
Filip Bartosiak <filipbart1@gmail.com>
2020-12-05 16:48:30 +0100

    Made a basic quiz app

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,68 @@
 import 'package:flutter/material.dart';
 
+import './quiz.dart';
+import './result.dart';
+
 void main() => runApp(QuizApp());
 
-class QuizApp extends StatelessWidget {
+class QuizApp extends StatefulWidget {
+  @override
+  _QuizAppState createState() => _QuizAppState();
+}
+
+class _QuizAppState extends State<QuizApp> {
+  final _questions = const [
+    {
+      'questionText': 'What\'s your favorite color?',
+      'answers': [
+        {'text': 'Black', 'score': 1},
+        {'text': 'Red', 'score': 5},
+        {'text': 'Green', 'score': 3},
+        {'text': 'White', 'score': 8}
+      ],
+    },
+    {
+      'questionText': 'What\'s your favorite animal?',
+      'answers': [
+        {'text': 'Rabbit', 'score': 3},
+        {'text': 'Snake', 'score': 1},
+        {'text': 'Elephant', 'score': 8},
+        {'text': 'Lion', 'score': 5}
+      ]
+    },
+    {
+      'questionText': 'What\'s your favorite music?',
+      'answers': [
+        {'text': 'Rock', 'score': 8},
+        {'text': 'Pop', 'score': 3},
+        {'text': 'Rap', 'score': 1},
+        {'text': 'R&B', 'score': 5},
+      ]
+    }
+  ];
+  var _questionIndex = 0;
+  var _totalScore = 0;
+
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
+  void _answerQuestion(int score) {
+    _totalScore += score;
+    setState(() {
+      ++_questionIndex;
+    });
+    print(_questionIndex);
+    if (_questionIndex < _questions.length) {
+      print('We have more questions!');
+    } else {
+      print('No more questions!');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -10,7 +70,13 @@ class QuizApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Quiz App'),
         ),
-        body: Text('Default text'),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+                questions: _questions,
+              )
+            : Result(_totalScore, _resetQuiz),
       ),
     );
   }
